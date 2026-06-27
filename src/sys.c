9d0e554965e651005249d81b9c78feebed363205
BlueIcicl <yes.nope0000@gmail.com>
2026-06-27 23:09:50 +0200

    min(assembler): change all format types for uint16_t to %u

--- src/sys.c
+++ src/sys.c
@@ -21,8 +21,14 @@ static void emit_x86_linux(string *dst, bf_call call, char *arg0, char *arg1, ch
         // error handling
         return;
     }
+    if (syscall == 0)
+    {
+        string_append_string(dst, "\txorl %eax, %eax\n");
+    } else
+    {
+        string_append_format(dst, "\tmovl $%d, %%eax\n", syscall);
+    }
 
-    string_append_format(dst, "\tmovl $%d, %%eax\n", syscall);
     if (arg0)
     {
         if (strncmp(arg0, "$0", 2) == 0) string_append_string(dst, "\txorl %ebx, %ebx\n");
@@ -105,12 +111,17 @@ static void emit_inc_op(arc_type type, string *dst, uint16_t count)
     {
         case ARC_X86_64_LINUX: {
             if (count == 1) string_append_string(dst, "\tincb (%rbx)\n");
-            else if (count > 1) string_append_format(dst, "\taddb $%i, (%%rbx)\n", count);
+            else if (count > 1) string_append_format(dst, "\taddb $%u, (%%rbx)\n", count);
 
             break;
         }
 
-        case ARC_X86_LINUX: break; // ...
+        case ARC_X86_LINUX: {
+            if (count == 1) string_append_string(dst, "\tincb (%edi)\n");
+            else if (count > 1) string_append_format(dst, "\taddb $%u, (%%edi)\n", count);
+
+            break;
+        }
     }
 }
 
@@ -122,12 +133,17 @@ static void emit_dec_op(arc_type type, string *dst, uint16_t count)
     {
         case ARC_X86_64_LINUX: {
             if (count == 1) string_append_string(dst, "\tdecb (%rbx)\n");
-            else if (count > 1) string_append_format(dst, "\tsubb $%i, (%%rbx)\n", count);
+            else if (count > 1) string_append_format(dst, "\tsubb $%u, (%%rbx)\n", count);
 
             break;
         }
 
-        case ARC_X86_LINUX: break; // ...
+        case ARC_X86_LINUX: {
+            if (count == 1) string_append_string(dst, "\tdecb (%edi)\n");
+            else if (count > 1) string_append_format(dst, "\tsubb $%u, (%%edi)\n", count);
+
+            break;
+        }
     }
 }
 
@@ -141,7 +157,8 @@ static void emit_clear_op(arc_type type, string *dst)
             break;
         }
         case ARC_X86_LINUX: {
-            break; // ...
+            string_append_string(dst, "\txorl %edi, %edi\n");
+            break;
         }
     }
 }
@@ -154,12 +171,17 @@ static void emit_lshift_op(arc_type type, string *dst, uint16_t count)
     {
         case ARC_X86_64_LINUX: {
             if (count == 1) string_append_string(dst, "\tdecq %rbx\n");
-            else if (count > 1) string_append_format(dst, "\tsubq $%d, %%rbx\n", count);
+            else if (count > 1) string_append_format(dst, "\tsubq $%u, %%rbx\n", count);
 
             break;
         }
 
-        case ARC_X86_LINUX: break; // ...
+        case ARC_X86_LINUX: {
+            if (count == 1) string_append_string(dst, "decl %edi\n");
+            else if (count > 1) string_append_format(dst, "\tsubl $%u, %%edi\n", count);
+
+            break;
+        }
     }
 }
 
@@ -171,12 +193,17 @@ static void emit_rshift_op(arc_type type, string *dst, uint16_t count)
     {
         case ARC_X86_64_LINUX: {
             if (count == 1) string_append_string(dst, "\tincq %rbx\n");
-            else if (count > 1) string_append_format(dst, "\taddq $%d, %%rbx\n", count);
+            else if (count > 1) string_append_format(dst, "\taddq $%u, %%rbx\n", count);
 
             break;
         }
 
-        case ARC_X86_LINUX: break; // ...
+        case ARC_X86_LINUX: {
+            if (count == 1) string_append_string(dst, "\tincl %edi\n");
+            else if (count > 1) string_append_format(dst, "\taddl $%u, %%edi\n", count);
+
+            break;
+        }
     }
 }
 
@@ -186,47 +213,61 @@ static void emit_loop_start(arc_type type, string *dst, uint16_t *loop_count, st
 
     switch (type)
     {
-        case ARC_X86_64_LINUX:
-        {
-            string_append_format(dst, ".L%d_start:\n", ++(*loop_count));
+        case ARC_X86_64_LINUX: {
+            string_append_format(dst, ".L%u_start:\n", ++(*loop_count));
             push(loop_stack, *loop_count);
             string_append_string(dst, "\tcmpb $0, (%rbx)\n");
-            string_append_format(dst, "\tje .L%d_end\n", *loop_count);
+            string_append_format(dst, "\tje .L%u_end\n", *loop_count);
             break;
         }
 
-        case ARC_X86_LINUX: break; // ...
+        case ARC_X86_LINUX: {
+            string_append_format(dst, ".L%u_start:\n", ++(*loop_count));
+            push(loop_stack, *loop_count);
+            string_append_string(dst, "\tcmpb $0, (%edi)\n");
+            string_append_format(dst, "\tje .L%u_end\n", *loop_count);
+            break;
+        }
     }
 }
 
-static void emit_loop_end(arc_type type, string *dst, stack *loop_stack)
+static void emit_loop_end(string *dst, stack *loop_stack)
 {
     if (!dst) return;
 
-    switch (type)
-    {
-        case ARC_X86_64_LINUX: {
-            uint16_t loop_count = pop(loop_stack);
-            string_append_format(dst, "\tjmp .L%d_start\n", loop_count);
-            string_append_format(dst, ".L%d_end:\n", loop_count);
-            break;
-        }
-
-        case ARC_X86_LINUX: break; // ...
-    }
+    uint16_t loop_count = pop(loop_stack);
+    string_append_format(dst, "\tjmp .L%u_start\n", loop_count);
+    string_append_format(dst, ".L%u_end:\n", loop_count);
 }
 
 static void emit_flush(arc_type type, string *dst, uint16_t *outs)
 {
     if (*outs == 0) return;
 
-    string *count_str = string_create(3);
-    string_append_format(count_str, "$%u", *outs);
-    string_append_string(dst, "\tlea print_buf(%rip), %rsi\n");
-    emit_syscall(type, dst, BF_CALL_WRITE, "$1", NULL, string_get_raw(count_str));
-    *outs = 0;
-    string_append_string(dst, "\txorq %rcx, %rcx\n");
-    string_destroy(count_str);
+    switch (type)
+    {
+        case ARC_X86_64_LINUX: {
+            string *count_str = string_create(3);
+            string_append_format(count_str, "$%u", *outs);
+            string_append_string(dst, "\tlea print_buf(%rip), %rsi\n");
+            emit_syscall(type, dst, BF_CALL_WRITE, "$1", NULL, string_get_raw(count_str));
+            *outs = 0;
+            string_append_string(dst, "\txorq %rcx, %rcx\n");
+            string_destroy(count_str);
+            break;
+        }
+
+        case ARC_X86_LINUX: {
+            string *count_str = string_create(3);
+            string_append_format(count_str, "$%u", *outs);
+            string_append_string(dst, "\tlea print_buf(%rip), %ecx\n");
+            emit_syscall(type, dst, BF_CALL_WRITE, "$1", NULL, string_get_raw(count_str));
+            *outs = 0;
+            string_append_string(dst, "\txorl %ecx, %ecx\n");
+            string_destroy(count_str);
+            break;
+        }
+    }
 }
 
 static void emit_out_op(arc_type type, string *dst, uint16_t *outs)
@@ -249,7 +290,17 @@ static void emit_out_op(arc_type type, string *dst, uint16_t *outs)
         }
 
         case ARC_X86_LINUX: {
-            // ....
+            if (*outs == 20)
+            {
+                emit_flush(type, dst, outs);
+            } else
+            {
+                string_append_string(dst, "\tmovb (%edi), %al\n");
+                string_append_string(dst, "\tmovb %al, print_buf(, %ecx, 1)\n");
+                string_append_string(dst, "\tincl %ecx\n");
+                (*outs)++;
+            }
+
             break;
         }
     }
@@ -269,7 +320,15 @@ static void emit_in_op(arc_type type, string *dst, uint16_t *outs)
             break;
         }
 
-        case ARC_X86_LINUX: break;
+        case ARC_X86_LINUX: {
+            if (*outs >= 1)
+            {
+                emit_flush(type, dst, outs);
+            }
+
+            emit_syscall(type, dst, BF_CALL_READ, "$0", "%edi", "$1");
+            break;
+        }
     }
 }
 
@@ -292,7 +351,7 @@ static void process_intructions(arc_type type, string *dst, dyn_array *operation
             case IR_LSHIFT: emit_lshift_op(type, dst, op.count); break;
             case IR_RSHIFT: emit_rshift_op(type, dst, op.count); break;
             case IR_LLOOP: emit_loop_start(type, dst, &loops, loop_stack); break;
-            case IR_RLOOP: emit_loop_end(type, dst, loop_stack); break;
+            case IR_RLOOP: emit_loop_end(dst, loop_stack); break;
             case IR_IN: emit_in_op(type, dst, &outs); break;
             case IR_OUT: emit_out_op(type, dst, &outs); break;
             case IR_END: emit_flush(type, dst, &outs); emit_syscall(type, dst, BF_CALL_EXIT, "$0", NULL, NULL); break;
@@ -305,6 +364,6 @@ static void emit_tape_reg_init(string *text, arc_type type)
     switch (type)
     {
         case ARC_X86_64_LINUX: string_append_string(text, "\tlea tape(%rip), %rbx\n"); break;
-        case ARC_X86_LINUX: break;
+        case ARC_X86_LINUX: string_append_string(text, "\tlea tape(%rip), %edi\n"); break;
     }
 }
