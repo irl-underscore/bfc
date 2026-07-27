008520a92b766fc24015b3bf9220d7dc8f0d64e8
BlueIcicl <yes.nope0000@gmail.com>
2026-07-27 09:58:30 +0200

    register: add table for reigsters

---
+++ src/tables/registers/register_end.h
@@ -0,0 +1,5 @@
+#undef X
+#undef _MATCH_X86_LINUX
+#undef _MATCH_X86_64_LINUX
+#undef _MATCH_ARM_LINUX
+#undef _MATCH_ARM64_LINUX
---
+++ src/tables/registers/register_tables.h
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2026 irl-underscore
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef REGISTER_TABLES_H
+#define REGISTER_TABLES_H
+
+typedef enum reg_type
+{
+    ASM_REG_TAPE_BASE,
+    ASM_REG_COUNT,
+    ASM_REG_PRINT_BUF,
+    ASM_REG_BYTE_BUFFER,
+    ASM_REG_CALL,
+    ASM_REG_PARAM0,
+    ASM_REG_PARAM1,
+    ASM_REG_PARAM2
+} reg_type;
+
+extern const char *x86_64_linux_register_table[];
+extern const char *x86_linux_register_table[];
+extern const char **register_table[];
+
+#endif /* REGISTER_TABLES_H */
